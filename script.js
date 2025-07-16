71e3828e2086649c625ca82700b20a4a0992b283
asyaress <athafarez2412@gmail.com>
2025-07-16 20:50:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 let lastOpenedModal = null;
 
 function swipeLeft() {
-    document.querySelector('.content').classList.add('swipe-left');
-    document.querySelector('.video-background').classList.add('swipe-left');
-    document.querySelector('.video-overlay').classList.add('swipe-left');
+    const welcomeScreen = document.querySelector('.welcome-screen');
+    welcomeScreen.classList.add('swipe-left');
 
     setTimeout(() => {
         const productContainer = document.getElementById('productContainer');
         productContainer.style.display = 'flex';
-
-        // Show modal and store it as last opened
         showModal();
-        lastOpenedModal = 'productModal'; // Track the modal ID in lastOpenedModal
-        sessionStorage.setItem('lastOpenedModal', lastOpenedModal); // Store in sessionStorage
-        document.querySelector('.content').style.display = 'none';
+        lastOpenedModal = 'productModal';
+        sessionStorage.setItem('lastOpenedModal', lastOpenedModal);
+        welcomeScreen.style.display = 'none';
     }, 1000);
 }
+
 function refreshPage() {
     location.reload(); // Perintah untuk reload halaman
   }
